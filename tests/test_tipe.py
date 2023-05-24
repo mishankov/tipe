91d79da11f394e98aaf7bda0e06c8ae7146495ee
mishankov <denis@mishankov.dev>
2023-05-24 18:18:17 +0300

    add test_generator

--- tests/test_tipe.py
+++ tests/test_tipe.py
@@ -7,3 +7,13 @@ def test_simple():
 
 def test_not_that_simple():
     assert Pipe([2, 3, 4]).pipe(len).pipe(lambda x: x + 1).pipe(float).unwrap() == 4.0
+
+
+def test_generator():
+    assert (
+        Pipe((i**i for i in range(5)))
+        .pipe(lambda x: (y for y in x))
+        .pipe(sum)
+        .unwrap()
+        == 289
+    )
