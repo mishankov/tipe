778a79cd9d31f5d8f4756dd693d5f9e50501cfda
Denis Mishankov <mishankov@mail.com>
2024-02-08 06:33:28 +0300

    add tests for check

--- tests/test_tipe.py
+++ tests/test_tipe.py
@@ -22,3 +22,14 @@ def test_additional_params():
 
     assert Pipe(1).pipe(add, 2).unwrap() == 3
     assert Pipe(2).pipe(range, 4).pipe(len).unwrap() == 2
+
+
+def test_check_run(capsys):
+    assert Pipe(1).check(print).unwrap() == 1
+
+    captured = capsys.readouterr()
+    assert captured.out == "1\n"
+
+
+def test_check_immutability():
+    assert Pipe(1).check(lambda x: x + 1).unwrap() == 1
